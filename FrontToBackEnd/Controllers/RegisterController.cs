4c83283ddf20fd1597b80a4ba1892d54be07a38a
Elmir9555 <elmiriu@code.edu.az>
2022-03-28 22:56:18 +0400

    createadminpanel

---
+++ FrontToBackEnd/Controllers/RegisterController.cs
@@ -0,0 +1,25 @@
+﻿using FrontToBackEnd.Data;
+using FrontToBackEnd.Models;
+using FrontToBackEnd.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBackEnd.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly AppDbContext _context;
+        public RegisterController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
